ab1889235677c0b02753e5601aacc0a7919b7bfc
Taylor Sarrafian <surprisetalk@gmail.com>
2016-07-20 16:34:00 +0000

    separated pagelets and pages

--- page.js
+++ page.js
@@ -1,16 +1,29 @@
 
+var _ = require('underscore');
+var pile = require('../aiport-pile/pile.js');
 var piler = require('../aiport-dev/dev.js').pile;
+// TODO: add recursive population to pagelet
 
 // TODO: can we get around this mongoose thing?
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
-// BUG: call stack exceeded when nesting > 1
-// pagelets, with recursive pagelets
-// var pagelet_schema = new Schema( { scrap: String, options: Schema.Types.Mixed } );
-// pagelet_schema.add( { pagelets: [ pagelet_schema ] } );
-// module.exports = piler( "page", { name: String, route: String, pagelets: [ pagelet_schema ] } );
+var schema = { name: String, 
+	       route: String, 
+	       pagelets: [ { type: Schema.Types.ObjectId,
+			     ref: "pagelet" } ] };
 
-// KLUDGE: we should use a recursive schema for pagelets 
-module.exports = piler( "page", { name: String, route: String, pagelets: [ Schema.Types.Mixed ] } );
+// TODO: this is slow
+var populator = pagelet_id =>
+    pile('pagelet').first({'_id':pagelet_id});
+
+var populatorer = page =>
+    Promise.all( page.pagelets.map( populator ) ).then( pagelets => _.extend( page, { pagelets: pagelets } ) );
+
+var controllers = model => ({
+    fetch: data => model.find( data ).then( pages => Promise.all( pages.map( populatorer ) ) )
+});
+
+// TODO: population
+module.exports = piler( "page", schema, controllers );
